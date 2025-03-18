15af1e85b99696f92557bc86a6f2ab392ef44ae1
victor <victor.mutuma@student.moringaschool.com>
2025-03-18 09:12:15 +0300

    save commited changes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hooper';
+
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, GRACE HOOPER!`;
+
+const shortGreeting = "Welcome, Grace!";
+
+
